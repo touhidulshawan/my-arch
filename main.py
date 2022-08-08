b40320185f40311afe2bc83e0d5165dc583e5197
Touhidul Shawan <touhidulshawan@gmail.com>
2022-08-08 18:11:48 +0600

    add function to clone repositories

--- main.py
+++ main.py
@@ -27,10 +27,37 @@ def install_apps():
         for app in apps_data:
             cprint(txt=f"[+] installing {app}", color="green")
             os.system(f"yay --noconfirm -S {app} ")
+        apps_data.close()
+    except Exception:
+        cprint(txt=Exception, color="red")
 
+
+# config github-cli
+
+
+def config_gh():
+    try:
+        cprint(txt="Configuration of github-cli", color="orange")
+        os.system("gh auth login")
+    except Exception:
+        cprint(txt=Exception, color="red")
+
+
+# clone my dotfiles from my github repositories
+
+
+def clone_dotfiles():
+    repo_names = open("./repository-names.txt", "r")
+    try:
+        os.chdir("/home/shawan/.config")
+        for repo in repo_names:
+            cprint(txt=f"Cloning {repo} from touhidulshawan/{repo}", color="green")
+            os.system(f"gh repo clone {repo}")
     except Exception:
         cprint(txt=Exception, color="red")
 
 
 install_yay()
 install_apps()
+config_gh()
+clone_dotfiles()
