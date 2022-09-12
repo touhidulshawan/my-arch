e3b9f8abe9aa84d9a21109f9df0248c1bdff49ae
Touhidul Shawan <touhidulshawan@gmail.com>
2022-09-12 21:56:38 +0600

    minor edit

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-mkdir $HOMR/dotfiles
+mkdir "$HOME/dotfiles"
 
-git clone --bare git@github.com:touhidulshawan/dotfiles.git $HOME/dotfiles
+git clone --bare git@github.com:touhidulshawan/dotfiles.git "$HOME/dotfiles"
 
-echo alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' >>$HOME/.bashrc
+echo alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' >>"$HOME/.bashrc"
 
-source ~/.bashrc
+source "$HOME/.bashrc"
 
 config checkout
 config config status.showUntrackedFiles no
