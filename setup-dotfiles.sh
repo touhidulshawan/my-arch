b42bce5be419f69738395535c28345b1090728e0
Touhidul Shawan <touhidulshawan@gmail.com>
2022-12-16 00:04:30 +0600

    dotfiles to .dotfiles

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-mkdir "$HOME/dotfiles"
+mkdir "$HOME/.dotfiles"
 
-git clone --bare git@github.com:touhidulshawan/dotfiles.git "$HOME/dotfiles"
+git clone --bare git@github.com:touhidulshawan/dotfiles.git "$HOME/.dotfiles"
 
 config() {
-    /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $@
+    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
 config checkout
